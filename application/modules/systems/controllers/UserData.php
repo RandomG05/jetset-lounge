993e61f3eb82c58ca6601a2fd765e3d2c88d0778
Taufik H <42793835+RandomG05@users.noreply.github.com>
2022-02-24 02:39:39 +0700

    Fixed user system

--- application/modules/systems/controllers/UserData.php
+++ application/modules/systems/controllers/UserData.php
@@ -6,6 +6,7 @@ class UserData extends MY_Controller {
 	function __construct()
 	{
 		parent::__construct();
+		$this->prefix = config_item('db_prefix');
 		$this->load->model('model_UserData', 'm');
 	}
 
